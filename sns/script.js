bd0a522df0223a9648aa90bead5d688d95aaa4c2
MangJang <mang061013@gmail.com>
2022-06-29 01:35:14 +0900

    commit

--- sns/script.js
+++ sns/script.js
@@ -1,4 +1,5 @@
 let moreView = true
+let accountMenu = true
 
 let item = document.querySelector(".item")
 item.addEventListener("click",
@@ -37,25 +38,96 @@ item.addEventListener("click",
             .textContent = "더보기"
             
             document.querySelector(".hide-guide a:nth-child(1)")
-            .style.display = "none"
+            .style.display = ""
             
             document.querySelector(".hide-guide a:nth-child(2)")
-            .style.display = "none"
+            .style.display = ""
             
             document.querySelector(".hide-guide a:nth-child(3)")
-            .style.display = "none"
+            .style.display = ""
             
             document.querySelector(".hide-guide a:nth-child(4)")
-            .style.display = "none"
+            .style.display = ""
             
             document.querySelector(".renderer")
-            .style.gridTemplateRows = "repeat(10, 40px)"
+            .style.gridTemplateRows = ""
             
             document.querySelector(".hide-guide")
-            .style.gridTemplateRows = " repeat(1, 40px)"
+            .style.gridTemplateRows = ""
 
             moreView = true
         }
+    }
+)
 
+window.addEventListener("resize", function() {
+    if(window.innerWidth <= 1320) {
+        document.querySelector(".item")
+        .style.display = "none"
+        if(true) {
+            document.querySelector(".item i")
+            .setAttribute("class", "fa fa-light fa-angle-down")
+            
+            document.querySelector(".item span")
+            .textContent = "더보기"
+            
+            document.querySelector(".shorts-guide-hide")
+            .style.display = ""
+            
+            document.querySelector(".renderer")
+            .style.gridTemplateRows = ""
+            
+            document.querySelector(".hide-guide")
+            .style.gridTemplateRows = ""
+            
+            document.querySelector(".hide-guide")
+            .style.display = "none"
+            
+            document.querySelector(".hide-guide a")
+            .style.display = "none"
+        }
+    }
+    if(window.innerWidth > 1320) {
+        document.querySelector(".renderer")
+        .style.gridTemplateRows = "repeat(11, 40px)"
+
+        document.querySelector(".item")
+        .style.display = "block"
+        if(moreView) {
+            document.querySelector(".hide-guide")
+            .style.gridTemplateRows = "repeat(1, 40px)"
+        }else if (!moreView){
+            document.querySelector(".renderer")
+            .style.gridTemplateRows = "repeat(10, 40px) 200px"
+
+            document.querySelector(".hide-guide")
+            .style.gridTemplateRows = "repeat(5, 40px)"
+            
+            document.querySelector(".hide-guide a:nth-child(1)")
+            .style.display = "block"
+            
+            document.querySelector(".item span")
+            .textContent = "간략히 보기"
+
+            document.querySelector(".item i")
+            .setAttribute("class", "fa fa-light fa-angle-up")
+        }
+        document.querySelector(".hide-guide")
+        .style.display = "block"
+    }
+})
+
+let profile = document.querySelector(".profile")
+profile.addEventListener("click",
+    function(e) {
+        if(accountMenu) {
+            document.querySelector(".account-menu")
+            .style.display = "block"
+            accountMenu = false
+        } else if(!accountMenu) {
+            document.querySelector(".account-menu")
+            .style.display = "none"
+            accountMenu = true
+        }
     }
 )
